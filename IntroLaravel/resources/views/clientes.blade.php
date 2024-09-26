d5c33d615094f91a3f213f82dd11ba6d9d41f0e1
AndyArrB <122043862@upq.edu.mx>
2024-09-25 18:25:16 -0600

    Avance 1 2509

--- IntroLaravel/resources/views/clientes.blade.php
+++ IntroLaravel/resources/views/clientes.blade.php
@@ -3,16 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    @vite(['resources/js/app.js'])
-
-    <title>Document</title>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Consultar clientes</title>
 </head>
 <body>
     
-
-<h1> Hola mundo </h1>
-
-
 </body>
 </html>
