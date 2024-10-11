2fed12d4bbff99d7e65492de70b0e167adcb8039
AndyArrB <122043862@upq.edu.mx>
2024-10-10 19:03:32 -0600

    Practica 4 Controladores 1010

--- IntroLaravel/routes/web.php
+++ IntroLaravel/routes/web.php
@@ -2,7 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 
- Route::get('/', function () {
+/* Importo el controlador para que el archivo lo reconozca y pueda ejecutarlo sin problema */
+use App\Http\Controllers\ControladorVistas;
+
+/* Ejecuto el controlador */
+Route::get('/', [ControladorVistas::class, 'home'])->name('rutainicio');
+
+Route::get('/form', [ControladorVistas::class, 'formulario'])->name('rutaform');
+
+Route::get('/consultar', [ControladorVistas::class, 'consulta'])->name('rutaclientes');
+
+/*  Route::get('/', function () {
     return view('inicio');
 })->name('rutainicio');
 
@@ -16,6 +26,7 @@ Route::get('/consultar', function () {
 
 Route::view('/', 'inicio')->name('rutainicio');
 Route::view('/form', 'formulario')->name('rutaform');
-Route::view('/consultar', 'clientes')->name('rutaclientes');
+Route::view('/consultar', 'clientes')->name('rutaclientes'); */
 
 Route::view('/component', 'componentes');
+
