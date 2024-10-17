103be86c5e1053407c346b79ca793022d438dabb
AndyArrB <122043862@upq.edu.mx>
2024-10-16 18:04:44 -0600

    Paso 3. Creación de vista al inicio - 1610

---
+++ practicaRepasos/resources/views/portada.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Portada</title>
+
+    @vite(['resources/js/app.js'])
+
+</head>
+<body>
+    
+      <div class="card container text-center mt-5" style="width: 45rem;">
+        <ul class="list-group list-group-flush fs-2">
+          <li class="list-group-item">Nombre: Andrea Arredondo Balderas</li>
+          <li class="list-group-item">Grupo: S193</li>
+          <li class="list-group-item">Materia: Programación web</li>
+          <li class="list-group-item">Profesor: Iván Isay Guerra López</li>
+        </ul>
+      </div>
+
+</body>
+</html>
